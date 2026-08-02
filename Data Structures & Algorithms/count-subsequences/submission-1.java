161050abdc95895025f147365bf8d961319dce5d
GowthamRajendra <71153965+GowthamRajendra@users.noreply.github.com>
2026-08-02 16:49:02 +0000

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.java
@@ -0,0 +1,41 @@
+class Solution {
+    int[][] memo;
+    int m;
+    int n;
+
+    public int numDistinct(String s, String t) {
+        n = s.length();
+        m = t.length();
+        memo = new int[n][m];
+
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < m; j++)
+            {
+                memo[i][j] = -1;
+            } 
+        }
+
+        return dfs(0, 0, s, t);
+    }
+
+    private int dfs(int i, int j, String text1, String text2)
+    {
+        if(n - i < m - j) return 0;
+
+        if (j == m) return 1;
+
+        if (i == n) return 0;
+
+        if (memo[i][j] != -1) return memo[i][j];
+
+        memo[i][j] = dfs(i+1, j, text1, text2);
+
+        if (text1.charAt(i) == text2.charAt(j))
+        {
+            memo[i][j] += dfs(i + 1, j + 1, text1, text2);   
+        }
+
+        return memo[i][j];
+    }
+}
